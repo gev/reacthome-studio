a2af8cfc360fc3fcaaea4ae9edfd0da59c9ffe69
Evgeny <gazdovsky@gmail.com>
2018-02-02 13:28:12 +0300

    more tests

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -13,7 +13,7 @@ let mainWindow;
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({ kiosk: true });
+    mainWindow = new BrowserWindow({  });
 
     // and load the index.html of the app.
     const startUrl = process.env.ELECTRON_START_URL || url.format({
