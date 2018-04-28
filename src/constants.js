3e4001a9e4f32c3577614ac7a6e49527e3126379
Evgeny <gazdovsky@gmail.com>
2018-04-28 20:07:05 +0300

    discovery action

--- src/constants.js
+++ src/constants.js
@@ -10,6 +10,8 @@ export const SERVICE_GROUP = '224.0.0.2';
 
 export const DISCOVERY_INTERVAL = 1000;
 
+export const ACTION_DISCOVERY = 0xf0;
+
 export const ACTION_GET = 'ACTION_GET';
 export const ACTION_SET = 'ACTION_SET';
 
