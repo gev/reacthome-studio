50a3401777c3b78ab1903218103d2b7b53738258
Evgeny <gazdovsky@gmail.com>
2019-10-09 15:01:19 +0300

    up

--- src/webrtc/handle.js
+++ src/webrtc/handle.js
@@ -30,12 +30,12 @@ export const onAction = (data, id) => (dispatch) => {
 
 export const onAsset = (data, id) => async () => {
   const buff = Buffer.from(data);
-  const transaction = buff.readBigInt32LE(0);
-  const total = buff.readUInt16LE(4);
-  const current = buff.readUInt16LE(6);
-  const length = buff.readUInt16LE(8);
-  const name = buff.slice(10, 10 + length).toString();
-  const chunk = buff.slice(10 + length);
+  const transaction = buff.readBigUInt64LE(0);
+  const total = buff.readUInt16LE(8);
+  const current = buff.readUInt16LE(10);
+  const length = buff.readUInt16LE(12);
+  const name = buff.slice(14, 14 + length).toString();
+  const chunk = buff.slice(14 + length);
   const temp = tmp(`${id}-${transaction}-${name}`);
   try {
     await appendFile(temp, chunk);
