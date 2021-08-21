c8f8b81f0bcfe336abc317bd682832e4bd20a764
Evgeny Gazdovsky <gazdovsky@gmail.com>
2021-08-21 16:05:19 +0300

    doppler raw

--- src/containers/Device/DeviceDoppler.js
+++ src/containers/Device/DeviceDoppler.js
@@ -98,17 +98,17 @@ class Doppler extends Component<PropsType> {
   };
 
   render() {
-    const { data } = this.state;
-    // const { raw = [] } = this.props;
-    // const data = {
-    //   labels: new Array(raw.length).fill(''),
-    //   datasets: [
-    //     {
-    //       ...art('raw'),
-    //       data: raw
-    //     }
-    //   ]
-    // };
+    // const { data } = this.state;
+    const { raw = [] } = this.props;
+    const data = {
+      labels: new Array(raw.length).fill(''),
+      datasets: [
+        {
+          ...art('raw'),
+          data: raw
+        }
+      ]
+    };
     return (
       <div className="paper">
         <div>
