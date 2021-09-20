739c8175ef6a4e1918ece7c8deb9874af72f43e3
Evgeny Gazdovsky <gazdovsky@gmail.com>
2021-09-20 15:54:55 +0300

    up

--- src/containers/Device/DeviceLanAmpZone.js
+++ src/containers/Device/DeviceLanAmpZone.js
@@ -63,6 +63,18 @@ class Container extends Component {
             <Row index={2} source={source[2]} config={this.config(2)} />
             <Row index={3} source={source[3]} config={this.config(3)} />
             <Row index={4} source={source[4]} config={this.config(4)} />
+            <Row index={5} source={source[5]} config={this.config(5)} />
+            <Row index={6} source={source[6]} config={this.config(6)} />
+            <Row index={7} source={source[7]} config={this.config(7)} />
+            <Row index={8} source={source[8]} config={this.config(8)} />
+            <Row index={9} source={source[9]} config={this.config(9)} />
+            <Row index={10} source={source[10]} config={this.config(10)} />
+            <Row index={11} source={source[11]} config={this.config(11)} />
+            <Row index={12} source={source[12]} config={this.config(12)} />
+            <Row index={13} source={source[13]} config={this.config(13)} />
+            <Row index={14} source={source[14]} config={this.config(14)} />
+            <Row index={15} source={source[15]} config={this.config(15)} />
+            <Row index={16} source={source[16]} config={this.config(16)} />
           </tbody>
         </table>
       </div>
