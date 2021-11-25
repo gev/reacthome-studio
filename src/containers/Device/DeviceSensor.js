3a65e87b1e7928c3d19e29c59846bdc084a56a66
Evgeny Gazdovsky <gazdovsky@gmail.com>
2021-11-25 21:31:56 +0300

    up

--- src/containers/Device/DeviceSensor.js
+++ src/containers/Device/DeviceSensor.js
@@ -5,6 +5,9 @@ import DeviceDoppler from './DeviceDoppler';
 import Di from './DeviceDiChannel';
 import RGB from '../RGB';
 import Display from '../Display';
+import Slider from '@rmwc/slider';
+import { send } from '../../websocket/peer';
+import { ACTION_TEMPERATURE_CORRECT, ACTION_VIBRO } from '../../constants';
 
 type Props = {
   temperature: ?number;
@@ -33,7 +36,7 @@ const Row = ({ title, value, magnitude }: RowProps) => (
 export default class extends Component<Props> {
   render() {
     const {
-      id, temperature, humidity, illumination, daemon, led, hasDoppler, hasDisplay
+      id, temperature, correct = 0, vibro = 100, humidity, illumination, daemon, led, hasDoppler, hasDisplay
     } = this.props;
     const rgb = (n) => {
       const a = [];
@@ -46,10 +49,37 @@ export default class extends Component<Props> {
       hasDisplay && <Display key="display" id={id} daemon={daemon} />,
       ...rgb(led),
       <table key="climate" style={{ textAlign: 'left' }}>
-        <tbody>
+				<tbody>
+					<tr>
+						<td class="paper">{`Correct ${correct}°C`}</td>
+						<td class="paper">
+							<Slider
+								value={correct}
+								min={-12.8}
+								max={12.7}
+								step={0.1}
+								onInput={(event) => {
+									send(daemon, {id, type: ACTION_TEMPERATURE_CORRECT, value: event.detail.value})
+								 }} />
+						</td>
+					</tr>
           <Row title="Temperature" value={temperature} magnitude="°C" />
           <Row title="Humidity" value={humidity} magnitude="%" />
           <Row title="Illumination" value={illumination} magnitude="lux" />
+					<tr>
+						<td class="paper">Vibro</td>
+						<td class="paper">
+							<Slider
+								value={vibro / 25}
+								min={0}
+								max={10}
+								step={1}
+								discrete
+								onInput={(event) => {
+									send(daemon, {id, type: ACTION_VIBRO, value: event.detail.value * 25})
+								 }} />
+						</td>
+					</tr>
         </tbody>
       </table>,
       <table key="buttons">
