8ef1df9f81d040c040201ef93a386d6007e96c2d
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-02-14 14:44:08 +0300

    refactor cardDi

--- src/containers/Details/Card/CardDi.js
+++ src/containers/Details/Card/CardDi.js
@@ -9,31 +9,22 @@ import {
   CardActions,
   CardActionIcons
 } from '@rmwc/card';
-import { TabBar, Tab } from '@rmwc/tabs';
 import { TextField } from '@rmwc/textfield';
 import { remove, modify, makeBind } from '../../../actions';
-import { CODE, DI } from '../../../constants';
+import { CODE } from '../../../constants';
 import Di from './CardDiBind';
 import SelectDi from './SelectDi';
 
 class Container extends Component {
-  state = { di: 0 }
-
-  selectDi = ({ detail: { index } }) => {
-    this.setState({ di: index });
-  }
-
   change = (event) => {
     const { change } = this.props;
     const { id, value } = event.target;
     change({ [id]: value });
   }
-
   select = (bind) => {
     const { id } = this.props;
     this.props.makeBind(id, bind);
   }
-  
   render() {
     const {
       code, project, bind, removeField
@@ -46,18 +37,13 @@ class Container extends Component {
         <div className="paper">
           <SelectDi id={bind} root={project} onSelect={this.select} />
         </div>
-        <TabBar
-          activeTabIndex={this.state.di}
-          onActivate={this.selectDi}
-        >
-          <Tab>1</Tab>
-          <Tab>2</Tab>
-          <Tab>3</Tab>
-          <Tab>4</Tab>
-        </TabBar>
-        <div className="paper">
-          <Di id={`${id}/${DI}/${this.state.di + 1}`} project={project} />
-        </div>
+        {
+          bind && (
+            <div className="paper">
+              <Di id={bind} project={project} />
+            </div>
+          )
+        }
         <CardActions>
           <CardActionIcons>
             <CardAction icon="remove" onClick={removeField} />
@@ -66,7 +52,7 @@ class Container extends Component {
       </Card>
     );
   }
-}
+} 
 
 export default connect(
   ({ pool }, { id }) => pool[id] || {},
