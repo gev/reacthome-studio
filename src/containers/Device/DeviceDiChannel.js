ced67f313297e2c860e30d737a77e8947fe2ca6b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-04-22 10:33:34 +0300

    up

--- src/containers/Device/DeviceDiChannel.js
+++ src/containers/Device/DeviceDiChannel.js
@@ -5,9 +5,18 @@ import React from 'react';
 import { DI } from '../../constants';
 import connect from './connect';
 
-export default connect(DI)(({ index, value, title }) => (
-  <div>
-    <div><Typography use="caption">{title || index}</Typography></div>
-    <div><Icon icon="fiber_manual_record" theme={value ? 'secondary' : 'text-hint-on-background'} /></div>
-  </div>
-));
+export default connect(DI)(({ index, value, title, timestamp }) => {
+  const date = new Date(timestamp);
+  return (
+    <div>
+      <div><Typography use="caption">{title || index}</Typography></div>
+      <div><Icon icon="fiber_manual_record" theme={value ? 'secondary' : 'text-hint-on-background'} /></div>
+      {timestamp && (
+        <div>
+          <div><Typography use="caption">{(new Date(timestamp)).toLocaleDateString()}</Typography></div>
+          <div><Typography use="caption">{(new Date(timestamp)).toLocaleTimeString()}</Typography></div>
+        </div>
+      )}
+    </div>
+  )
+})
