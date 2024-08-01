c9adf21485f287bc9806a95520d0d470f751be07
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-08-02 01:33:14 +0300

    up

--- build.js
+++ build.js
@@ -1,6 +1,8 @@
 
 const os = require('os');
 const build = require('electron-packager');
+const path = require('path');
+const fs = require('fs');
 
 const icon = () => {
   switch (os.platform()) {
@@ -30,6 +32,31 @@ build({
     'webpack.*',
     'yarn.*'
   ],
+  afterCopy: [
+    (buildPath, electronVersion, platform, arch, callback) => {
+      [
+        "./node_modules/@electron/remote/dist/src/common",
+        "./node_modules/@electron/remote/dist/src/main",
+        "./node_modules/@electron/remote/main",
+      ].forEach(dir => {
+        fs.mkdirSync(path.join(buildPath, dir), { recursive: true });
+      });
+      [
+        "./node_modules/@electron/remote/dist/src/common/get-electron-binding.js",
+        "./node_modules/@electron/remote/dist/src/common/ipc-messages.js",
+        "./node_modules/@electron/remote/dist/src/common/module-names.js",
+        "./node_modules/@electron/remote/dist/src/common/type-utils.js",
+        "./node_modules/@electron/remote/dist/src/main/objects-registry.js",
+        "./node_modules/@electron/remote/dist/src/main/server.js",
+        "./node_modules/@electron/remote/dist/src/main/index.js",
+        "./node_modules/@electron/remote/main/index.js",
+      ].forEach(file => {
+        fs.copyFileSync(path.join(__dirname, file), path.join(buildPath, file));
+      });
+      callback();
+    }
+  ],
+  asar: true,
   overwrite: true,
   icon: icon()
 });
