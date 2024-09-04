4f4952d89402c5754ce0ef6facb05b09faa81e39
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-09-04 13:12:20 +0300

    up

--- src/containers/Device/DeviceServer.js
+++ src/containers/Device/DeviceServer.js
@@ -1,11 +1,11 @@
 
 import { Tab, TabBar } from '@rmwc/tabs';
 import React, { Component } from 'react';
+import DeviceALED from './DeviceALED';
 import DeviceDi from './DeviceDi';
 import DeviceDimmer from './DeviceDimmer';
 import DeviceExt from './DeviceExt';
 import DeviceRSHub4 from './DeviceRSHub4l';
-import DeviceALED from './DeviceALED';
 
 
 export default class extends Component {
@@ -13,19 +13,26 @@ export default class extends Component {
   select = ({ detail: { index } }) => {
     this.setState({ tabIndex: index });
   }
+
   render() {
     const { tabIndex } = this.state;
+    const { id, version } = this.props;
+    const major = parseInt(version.split('.')[0], 10);
+    const tabs = [
+      <Tab key={`${id}/rbus`}>RBUS</Tab>,
+      <Tab key={`${id}/inputs`}>Inputs</Tab>,
+      <Tab key={`${id}/dimmer`}>Dimmer</Tab>,
+      <Tab key={`${id}/ext`}>Ext</Tab>,
+    ];
+    if (major >= 5) {
+      tabs.push(<Tab key={`${id}/aled`}>ALED</Tab>);
+    }
     return ([
       <div key="tab">
         <TabBar
           activeTabIndex={tabIndex}
           onActivate={this.select}
-        >
-          <Tab>RBUS</Tab>
-          <Tab>Inputs</Tab>
-          <Tab>Dimmer</Tab>
-          <Tab>Ext</Tab>
-          <Tab>ALED</Tab>
+        >{tabs}
         </TabBar>
       </div>,
       <div key="body">
