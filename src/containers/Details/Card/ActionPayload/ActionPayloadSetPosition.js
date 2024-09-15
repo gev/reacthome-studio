247702a19fd364a0e0aaa4fc59ca87aacc5a2001
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2024-09-15 16:58:56 +0300

    up

---
+++ src/containers/Details/Card/ActionPayload/ActionPayloadSetPosition.js
@@ -0,0 +1,24 @@
+
+import React, { Component } from 'react';
+import Autocomplete from '../../../Filter';
+import Dimmer from './Dimmer';
+import Position from './Position';
+
+export default class extends Component {
+  select = (id) => {
+    const { change, payload } = this.props;
+    change({ payload: { ...payload, id } });
+  }
+
+  render() {
+    const { id, root, payload = {} } = this.props;
+    return [
+      <div id="id" className="paper">
+        <Autocomplete id={payload.id} root={root} onSelect={this.select} />
+      </div>,
+      <div key="value" className="paper">
+        <Position action={id} payload={payload} />
+      </div>
+    ];
+  }
+}
---
+++ src/containers/Details/Card/ActionPayload/Position.js
@@ -0,0 +1,34 @@
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { modify } from '../../../../actions';
+import Slider from '../../../../components/Slider';
+
+
+class Container extends Component {
+  input = (event) => {
+    this.props.modify(event.detail.value);
+  }
+
+  render() {
+    const { value } = this.props;
+    return (
+      <Slider
+        min={0}
+        max={100}
+        step={1}
+        value={value}
+        onInput={this.input}
+        discrete
+      />
+    );
+  }
+}
+
+export default connect(
+  (state, { payload }) => payload,
+  (dispatch, { action, payload }) => bindActionCreators({
+    modify: (value) => modify(action, { payload: { ...payload, value } }),
+  }, dispatch)
+)(Container);
